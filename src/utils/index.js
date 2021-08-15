b4fa7c0d386e1640e8d10b1aed7dfd54bf23209a
hefeng6500 <2443992009@qq.com>
2021-08-15 18:43:07 +0800

    stack 应用 进制转换

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,3 +14,40 @@ export function defaultCompare(a, b) {
   return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
 }
 
+// 10 进制转 2 进制
+export function decimalToBinary(decNumber) {
+  const remStack = new Stack();
+  let number = decNumber;
+  let rem;
+  let binaryString = "";
+  while (number > 0) {
+    rem = Math.floor(number % 2);
+    remStack.push(rem);
+    number = Math.floor(number / 2);
+  }
+  while (!remStack.isEmpty()) {
+    binaryString += remStack.pop().toString();
+  }
+  return binaryString;
+}
+
+// 10 进制转 2~36 进制
+export function baseConverter(decNumber, base) {
+  const remStack = new Stack();
+  const digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // {6}
+  let number = decNumber;
+  let rem;
+  let baseString = "";
+  if (!(base >= 2 && base <= 36)) {
+    return "";
+  }
+  while (number > 0) {
+    rem = Math.floor(number % base);
+    remStack.push(rem);
+    number = Math.floor(number / base);
+  }
+  while (!remStack.isEmpty()) {
+    baseString += digits[remStack.pop()]; // {7}
+  }
+  return baseString;
+}
