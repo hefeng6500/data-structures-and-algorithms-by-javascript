fedd404aa06bd92bef8217e33b1e451a3a531389
hefeng6500 <2443992009@qq.com>
2021-08-22 21:33:44 +0800

    学习 二叉搜索树
    - 插入键
    - 中序遍历
    - 先序遍历
    - 后序遍历
    - 最小值
    - 最大值
    - 搜索二叉搜素树

--- src/code/19.BinarySearchTree.js
+++ src/code/19.BinarySearchTree.js
@@ -39,23 +39,88 @@ export class BinarySearchTree {
     }
   }
 
-  // search(key)：在树中查找一个键。如果节点存在，则返回 true；如果不存在，则返回 false。
-  search(key) {}
-
   // inOrderTraverse()：通过中序遍历方式遍历所有节点。
-  inOrderTraverse() {}
+  inOrderTraverse(callback) {
+    this.inOrderTraverseNode(this.root, callback);
+  }
+
+  inOrderTraverseNode(node, callback) {
+    if (node != null) {
+      this.inOrderTraverseNode(node.left, callback);
+      callback(node.key);
+      this.inOrderTraverseNode(node.right, callback);
+    }
+  }
 
   // preOrderTraverse()：通过先序遍历方式遍历所有节点。
-  preOrderTraverse() {}
+  preOrderTraverse(callback) {
+    this.preOrderTraverseNode(this.root, callback);
+  }
+
+  preOrderTraverseNode(node, callback) {
+    if (node != null) {
+      callback(node.key);
+      this.preOrderTraverseNode(node.left, callback);
+      this.preOrderTraverseNode(node.right, callback);
+    }
+  }
 
   // postOrderTraverse()：通过后序遍历方式遍历所有节点。
-  postOrderTraverse() {}
+  postOrderTraverse(callback) {
+    this.postOrderTraverseNode(this.root, callback);
+  }
+
+  postOrderTraverseNode(node, callback) {
+    if (node != null) {
+      this.postOrderTraverseNode(node.left, callback);
+      this.postOrderTraverseNode(node.right, callback);
+      callback(node.key);
+    }
+  }
 
   // min()：返回树中最小的值/键。
-  min() {}
+  min() {
+    return this.minNode(this.root);
+  }
+
+  minNode(node) {
+    let current = node;
+    while (current.left != null) {
+      current = current.left;
+    }
+    return current.key;
+  }
 
   // max()：返回树中最大的值/键。
-  max() {}
+  max() {
+    return this.maxNode(this.root);
+  }
+
+  maxNode(node) {
+    let current = node;
+    while (current.right != null) {
+      current = current.right;
+    }
+    return current.key;
+  }
+
+  // search(key)：在树中查找一个键。如果节点存在，则返回 true；如果不存在，则返回 false。
+  search(key) {
+    return this.searchNode(this.root, key);
+  }
+
+  searchNode(node, key) {
+    if (node == null) {
+      return false;
+    }
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      return this.searchNode(node.left, key);
+    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+      return this.searchNode(node.right, key);
+    } else {
+      return true;
+    }
+  }
 
   // remove(key)：从树中移除某个键
   remove(key) {}
