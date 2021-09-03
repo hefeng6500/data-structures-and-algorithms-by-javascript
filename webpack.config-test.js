d1ac2e42062b6aca7d27eafad00a0567c568ed5d
hefeng6500 <2443992009@qq.com>
2021-09-03 20:26:21 +0800

    删除无效文件

--- webpack.config-test.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require("webpack-merge");
-const nodeExternals = require("webpack-node-externals");
-const baseWebpackConfig = require("./webpack.config.js");
-
-module.exports = merge(baseWebpackConfig, {
-  target: 'node',
-  externals: [nodeExternals()],
-  devtool: "inline-cheap-module-source-map"
-});
