78ce82bda31e60dc161ba0fd64d4cc578e93db28
hefeng6500 <2443992009@qq.com>
2021-09-03 22:45:23 +0800

    add jest

--- webpack.config.test.js
+++
@@ -1,7 +0,0 @@
-const { merge } = require("webpack-merge");
-const nodeExternals = require("webpack-node-externals");
-const baseWebpackConfig = require("./webpack.config.js");
-
-module.exports = merge(baseWebpackConfig, {
-  externals: [nodeExternals()],
-});
