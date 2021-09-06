9289e6f8ba65b038394590c618619176f9748fe1
hefeng6500 <2443992009@qq.com>
2021-09-06 21:59:35 +0800

    feat: 堆排序

--- src/sort/heap-sort.js
+++ src/sort/heap-sort.js
@@ -1,5 +1,6 @@
 import { defaultCompare, swap } from "../utils/index.js";
 
+// 堆化函数
 function heapify(array, index, heapSize, compareFn) {
   let largest = index;
   const left = 2 * index + 1;
@@ -16,6 +17,7 @@ function heapify(array, index, heapSize, compareFn) {
   }
 }
 
+// 构建最大堆
 function buildMaxHeap(array, compareFn) {
   for (let i = Math.floor(array.length / 2); i >= 0; i--) {
     heapify(array, i, array.length, compareFn);
@@ -26,8 +28,10 @@ function buildMaxHeap(array, compareFn) {
 export default function heapSort(array, compareFn = defaultCompare) {
   let heapSize = array.length;
   buildMaxHeap(array, compareFn);
-  debugger
   
+  // 交换最大堆中元素，每次交换最大堆根元素和最后一个元素
+  // 并减小堆长度
+  // 不断交换直至堆大小为 1，最大堆完成所有交换，排序完成
   while (heapSize > 1) {
     swap(array, 0, --heapSize);
     heapify(array, 0, heapSize, compareFn);
