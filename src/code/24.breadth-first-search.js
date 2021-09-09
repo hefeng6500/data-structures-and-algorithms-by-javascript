31b0a9dfaabdca1f55cbe24b854c26965598d621
hefeng6500 <2443992009@qq.com>
2021-09-09 22:09:38 +0800

    feat: 图的遍历，广度优先

---
+++ src/code/24.breadth-first-search.js
@@ -0,0 +1,50 @@
+import { Queue } from "../code/11.Queue";
+
+const Colors = {
+  WHITE: 0,
+  GREY: 1,
+  BLACK: 2,
+};
+
+const initializeColor = (vertices) => {
+  const color = {};
+
+  for (let i = 0; i < vertices.length; i++) {
+    color[vertices[i]] = Colors.WHITE;
+  }
+
+  return color;
+};
+
+export const breadthFirstSearch = (graph, startVertex, callback) => {
+  const vertices = graph.getVertices();
+  const adjList = graph.getAdjList();
+  const color = initializeColor(vertices);
+  const queue = new Queue();
+
+  queue.enqueue(startVertex);
+
+  while (!queue.isEmpty()) {
+    const u = queue.dequeue();
+    const neighbors = adjList.get(u);
+
+    color[u] = Colors.GREY;
+
+    // 将 u 元素的字典数据全部访问完成，再将 u 设置为完全访问(black)
+    for (let i = 0; i < neighbors.length; i++) {
+      const w = neighbors[i];
+
+      // 如果 u 字典里面的元素没有被访问过，则设置为访问过（灰色），再将该元素进入队列
+      if (color[w] === Colors.WHITE) {
+        color[w] = Colors.GREY;
+        queue.enqueue(w);
+      }
+    }
+
+    color[u] = Colors.BLACK;
+
+    if (callback) {
+      callback(u);
+    }
+  }
+};
